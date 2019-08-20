015b367e7984487bc7bc8cb784aa3fd7e717e74f
apope8 <apope8@gmail.com>
2019-08-20 22:40:33 +0100

    Create Mockito test class

---
+++ OrderProcessingService/test/com/apope/order/bo/OrderBOImplTest.java
@@ -0,0 +1,37 @@
+package com.apope.order.bo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+
+import java.sql.SQLException;
+
+import com.apope.order.dao.OrderDAO;
+import com.apope.order.dto.Order;
+
+public class OrderBOImplTest {
+	
+	@Mock
+	OrderDAO dao;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void placeOrder_Should_Create_An_Order() throws SQLException {
+		
+		OrderBOImpl bo = new OrderBOImpl();
+		
+		bo.setDao(dao);
+		
+		Order order = new Order();
+		when(dao.create(order)).thenReturn(new Integer(1));
+	}
+
+}
